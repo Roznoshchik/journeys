1cb4f679e95071d667d2a2e5cae7559e323dd93b
RR <roznoshchik@gmail.com>
2024-01-04 14:04:28 +0200

    addresses are validated and coordinates retrieved
    returned addresses are rendered to screen allowing user to make a selection. The chosen items coordinates are then captured in a top level dictionary that will also store all the form results.

--- functions/app/utils/mapbox.py
+++ functions/app/utils/mapbox.py
@@ -1,7 +1,25 @@
-import json
 import os
+import requests
+from urllib.parse import quote
 
 
 def get_geocoded_suggestions(address):
     MAPBOX_API_KEY = os.environ.get("MAPBOX_API_KEY")
-    return json.dumps({"hello": address})
+    address = quote(address)
+    url = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{address}.json"
+    params = {
+        "access_token": MAPBOX_API_KEY,
+        "types": "country,region,district,place,locality",
+    }
+    try:
+        response = requests.get(url, params=params, timeout=15)
+        if response.status_code == 200:
+            return response.json().get("features")
+        elif response.status_code == 429:
+            print("Rate limit exceeded. Please try again later.")
+        else:
+            print(f"Error: {response.status_code} - {response.reason}")
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+
+    return None
