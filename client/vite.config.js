1f59002813e0e5c9506c22d5bdf145c9c0795e90
RR <roznoshchik@gmail.com>
2024-01-07 22:12:29 +0200

    npm build script now uses regex to replace static url

--- client/vite.config.js
+++ client/vite.config.js
@@ -42,6 +42,23 @@ export default defineConfig({
           }
         });
 
+        // Read the copied index.html and update static urls.
+        fs.readFile(templatesDestination, 'utf8', function(err, data) {
+          if (err) {
+            return console.log(err);
+          }
+          // Regular expression to match 'static/**.**'
+          const regex = /static\/(.*?)\.(js|css|jpg|png|gif|svg)/g;
+          // Replace found instances with the Flask url_for syntax
+          const result = data.replace(regex, "{{ url_for('static', filename='$1.$2') }}");
+
+          // Write the result to the same file
+          fs.writeFile(templatesDestination, result, 'utf8', function(err) {
+            if (err) return console.log(err);
+          });
+        });
+
+
         // Delete the index.html in static directory if it exists
         if (fs.existsSync(staticIndexHtml)) {
           fs.unlink(staticIndexHtml, function(err) {
