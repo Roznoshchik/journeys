8e00bc9af3f1fffdf665097e8a6075c067eb7e9e
RR <roznoshchik@gmail.com>
2024-02-05 13:37:43 +0000

    Images added to form, random lon/lat func created for testing.

--- functions/app/app.py
+++ functions/app/app.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, abort, jsonify
 from http import HTTPStatus
 from dotenv import load_dotenv
+import random
 
 from .utils.flasklambda import FlaskLambda
 from .utils.mapbox import get_geocoded_suggestions
@@ -22,9 +23,29 @@ def get_address_suggestions():
     if not address:
         abort(HTTPStatus.BAD_REQUEST, description="No address supplied")
 
-    suggestions = get_geocoded_suggestions(address)
+    suggestions = get_geocoded_suggestions_temp(address)
 
     return jsonify(suggestions), HTTPStatus.OK
+
+
+def get_random_lat_long():
+    """
+    Returns a random latitude and longitude as a tuple (float, float).
+    """
+    lat = random.uniform(-90, 90)  # Random between -90 and 90 degrees # nosec B311
+    lon = random.uniform(-180, 180)  # Random between -180 and 180 degrees # nosec B311
+
+    return (lat, lon)
+
+
+def get_geocoded_suggestions_temp(address):
+    suggestions = [
+        {
+            "place_name": address,
+            "center": get_random_lat_long(),
+        }
+    ]
+    return suggestions
 
 
 @app.errorhandler(HTTPStatus.NOT_FOUND)
