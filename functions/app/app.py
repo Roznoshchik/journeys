8d03e457e1318c1732989c09e3bfc647cbc5404e
RR <roznoshchik@gmail.com>
2024-02-07 11:35:15 +0000

    map functions moved to map.js and random_suggestions improved

--- functions/app/app.py
+++ functions/app/app.py
@@ -1,10 +1,9 @@
 from flask import render_template, request, abort, jsonify
 from http import HTTPStatus
 from dotenv import load_dotenv
-import random
 
 from .utils.flasklambda import FlaskLambda
-from .utils.mapbox import get_geocoded_suggestions
+from .utils.mapbox import get_geocoded_suggestions, get_n_random_suggestions
 
 
 load_dotenv()
@@ -23,29 +22,10 @@ def get_address_suggestions():
     if not address:
         abort(HTTPStatus.BAD_REQUEST, description="No address supplied")
 
-    suggestions = get_geocoded_suggestions_temp(address)
+    suggestions = get_n_random_suggestions(n=2)
+    # suggestions = get_geocoded_suggestions(address)
 
     return jsonify(suggestions), HTTPStatus.OK
-
-
-def get_random_lat_long():
-    """
-    Returns a random latitude and longitude as a tuple (float, float).
-    """
-    lat = random.uniform(-90, 90)  # Random between -90 and 90 degrees # nosec B311
-    lon = random.uniform(-180, 180)  # Random between -180 and 180 degrees # nosec B311
-
-    return (lat, lon)
-
-
-def get_geocoded_suggestions_temp(address):
-    suggestions = [
-        {
-            "place_name": address,
-            "center": get_random_lat_long(),
-        }
-    ]
-    return suggestions
 
 
 @app.errorhandler(HTTPStatus.NOT_FOUND)
